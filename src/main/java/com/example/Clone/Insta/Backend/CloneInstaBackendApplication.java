794071bf73191c34d21d3cd4f930d5218a63918e
Vinayak Kumbhar <kumbharvinayak751@gmail.com>
2026-09-24 18:22:21 +0530

    Initial-commit

---
+++ src/main/java/com/example/Clone/Insta/Backend/CloneInstaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Clone.Insta.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneInstaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneInstaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Clone/Insta/Backend/CloneInstaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Clone.Insta.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneInstaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
